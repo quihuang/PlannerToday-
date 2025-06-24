afd81da8d5b41ee3f73847ba943ea8a5a1405f4a
Franklin German Quihuang <quihuang2017@gmail.com>
2025-06-24 00:03:15 -0500

    Archivos necesarios para la generacion de la apk

---
+++ android/app/src/main/java/com/plannertoday/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.plannertoday.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.plannertoday.app',
+  appName: 'PlannerToday',
+  webDir: 'www'
+};
+
+export default config;
